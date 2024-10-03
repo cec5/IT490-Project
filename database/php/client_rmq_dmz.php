d27336b97c6e1f9eac386b7e0b8f0b7fb425ce41
cec5 <chris06.cortez@gmail.com>
2024-10-03 12:05:13 -0400

    Removed Unncessary Files, added database listener with frontend testscript

--- database/php/client_rmq_dmz.php
+++ database/php/client_rmq_dmz.php
@@ -23,7 +23,4 @@ function createRabbitMQClientDMZ($request){
 
 	return $response;
 }
-
-//uncomment below and run the file to see if it communicates correctly on the rabbitMQ server
-//createRabbitMQClientDMZ("test");
 ?>
