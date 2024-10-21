cfaa03e661962d315dd1a2105fb7240cb403b8a4
dhb-9 <dhb9@njit.edu>
2024-10-21 12:47:03 -0400

    updated sql file to include matches table & included simple page that when displayed will updated matches table

--- database/sql/table.sql
+++ database/sql/table.sql
@@ -1,8 +1,18 @@
+/*table that stores users*/
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(50) NOT NULL UNIQUE,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
 
+/*table that stores simple info from matches*/
+CREATE TABLE matches (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATETIME NOT NULL,
+    home_team VARCHAR(100) NOT NULL,
+    away_team VARCHAR(100) NOT NULL,
+    score_home TINYINT,
+    score_away TINYINT
 );
