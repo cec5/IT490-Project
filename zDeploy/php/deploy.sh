2cb9bfeea00e90e0d41f7e21ab1d51749914f6a8
Tomas Gonzalez <tpg3@njit.edu>
2024-11-20 00:56:09 -0500

    prototype for script to deploy

---
+++ zDeploy/php/deploy.sh
@@ -0,0 +1,25 @@
+DEPLOY_SERVER="172.24.193.68"
+DEPLOY_USER="deployer"                 
+FRONTEND_PATH="../../frontend"           
+BACKEND_PATH="../../backend"   
+DMZ_PATH="../../dmz"   
+DEPLOY_PATH="/home/user/IT490_Project/zDeploy" 
+PACKAGE_NAME="code_package_$(date +%Y%m%d_%H%M%S).tar.gz"
+
+#Function to check the success of a command
+check_success() {
+  if [ $? -ne 0 ]; then
+    echo "Error: $1"
+    exit 1
+  fi
+}
+
+#Bundle the code into a tar.gz package. Testing with only frontend first.
+echo "Bundling code from $FRONTEND_PATH into $PACKAGE_NAME..."
+tar -czf $PACKAGE_NAME -C "$FRONTEND_PATH" .
+check_success "Failed to create package."
+
+#SCP the package to the deployment server
+echo "Transferring package to $DEPLOY_SERVER:$DEPLOY_PATH..."
+scp -i $SSH_KEY $PACKAGE_NAME $DEPLOY_USER@$DEPLOY_SERVER:$DEPLOY_PATH
+check_success "Failed to transfer package to the deployment server."
